025c670263fea75036eb696ee135652c8922f674
lichenhao123 <3523388650@qq.com>
2018-12-14 16:52:15 +0800

    Commit Message

---
+++ src/org/westos/day21/herodemo/TestHero.java
@@ -0,0 +1,26 @@
+package org.westos.day21.herodemo;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class TestHero {
+    public static void main(String[] args) {
+
+    }
+
+}
+
+
+        // 1. 找到武将中武力前三的hero对象, 提示流也可以排序
+        // 2. 按出生地分组
+        // 3. 找出寿命前三的武将
+        // 4. 女性寿命最高的
+        // 5. 找出武力排名前三  100, 99, 97 97 ==> 4个人 吕布", "张飞", "关羽", "马超
+        // 6. 按各个年龄段分组： 0~20, 2140, 41~60, 60以上
+        // 7. 按武力段分组： >=90， 80~89, 70~79, <70
+        // 8. 按出生地分组后，统计各组人数
+
